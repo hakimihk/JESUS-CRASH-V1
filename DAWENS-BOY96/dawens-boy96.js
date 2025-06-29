a60ac50236d126c0d7dcbe2e09425509095f4150
Dapanel-gg <dawenstech@gmail.com>
2025-06-29 17:15:09 -0400

    Delete DAWENS-BOY96/dawens-boy96.js

--- DAWENS-BOY96/dawens-boy96.js
+++
@@ -1,6 +0,0 @@
-const anticallHandler = require('./handler/anticallHandler'); // adjust path
-
-// after connection established and `conn` is ready:
-conn.ev.on('call', async (update) => {
-  await anticallHandler(conn, update);
-});
